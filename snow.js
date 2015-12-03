04ea48cc5b33e7c4644c19d5190adbd4b0ad3eed
Kurisu Brooks <kurisubrooks@users.noreply.github.com>
2015-12-03 18:01:20 +1100

    Fixing Overflow

--- snow.js
+++ snow.js
@@ -44,7 +44,7 @@ function randomise(range) {
 
 function initSnow() {
 	var snowSize = snowMaxSize - snowMinSize;
-	marginBottom = document.body.scrollHeight - 5;
+	marginBottom = document.body.scrollHeight - 15;
 	marginRight = document.body.clientWidth - 15;
 
 	for (i = 0; i <= snowMax; i++) {
